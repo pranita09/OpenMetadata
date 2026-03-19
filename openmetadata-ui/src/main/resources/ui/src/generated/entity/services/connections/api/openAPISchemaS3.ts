9e8de645241945e05105ee8221f9b8ae617d7c7b
harshsoni2024 <64592571+harshsoni2024@users.noreply.github.com>
2026-03-19 09:59:14 +0530

    Feat-26065 : Add s3 file support in rest connector (#26504)

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/services/connections/api/openAPISchemaS3.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/services/connections/api/openAPISchemaS3.ts
@@ -11,84 +11,18 @@
  *  limitations under the License.
  */
 /**
- * REST Connection Config
- */
-export interface RESTConnection {
-    /**
-     * Regex to only fetch api collections with names matching the pattern.
-     */
-    apiCollectionFilterPattern?: FilterPattern;
-    /**
-     * Regex to only fetch api endpoints with names matching the pattern.
-     */
-    apiEndpointFilterPattern?: FilterPattern;
-    /**
-     * Documentation URL for the schema.
-     */
-    docURL?: string;
-    /**
-     * OpenAPI Schema source config. Either a URL or a file path must be provided.
-     */
-    openAPISchemaConnection: OpenAPISchemaConnection;
-    /**
-     * Supports Metadata Extraction.
-     */
-    supportsMetadataExtraction?: boolean;
-    /**
-     * Generated Token to connect to OpenAPI Schema.
-     */
-    token?: string;
-    /**
-     * REST API Type
-     */
-    type?: RESTType;
-}
-
-/**
- * Regex to only fetch api collections with names matching the pattern.
- *
- * Regex to only fetch entities that matches the pattern.
- *
- * Regex to only fetch api endpoints with names matching the pattern.
- */
-export interface FilterPattern {
-    /**
-     * List of strings/regex patterns to match and exclude only database entities that match.
-     */
-    excludes?: string[];
-    /**
-     * List of strings/regex patterns to match and include only database entities that match.
-     */
-    includes?: string[];
-}
-
-/**
- * OpenAPI Schema source config. Either a URL or a file path must be provided.
- *
- * Open API Schema URL Connection Config
- *
- * Open API Schema File Path Connection Config
- *
  * Open API Schema S3 Connection Config
  */
-export interface OpenAPISchemaConnection {
-    /**
-     * Open API Schema URL.
-     */
-    openAPISchemaURL?: string;
-    /**
-     * Path to a local OpenAPI schema file.
-     */
-    openAPISchemaFilePath?: string;
+export interface OpenAPISchemaS3 {
     /**
      * AWS credentials required to access the S3 file.
      */
-    awsCredentials?: AWSCredentials;
+    awsCredentials: AWSCredentials;
     /**
      * S3 URL of the OpenAPI schema file (JSON or YAML). Example:
      * https://bucket-name.s3.amazonaws.com/path/to/openapi_schema.json
      */
-    openAPISchemaS3URL?: string;
+    openAPISchemaS3URL: string;
 }
 
 /**
@@ -143,13 +77,4 @@ export interface AWSCredentials {
      * The name of a profile to use with the boto session.
      */
     profileName?: string;
-}
-
-/**
- * REST API Type
- *
- * REST API type
- */
-export enum RESTType {
-    REST = "Rest",
 }
